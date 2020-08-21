67dc5806a59d2ba3a0aca56200780351c6f6d121
David-Almaguer-La-Rosa <davideconomia83@gmail.com>
2020-08-21 13:44:26 +0200

    Página ejemplo lista

--- Clase_2.R
+++ Clase_2.R
@@ -5,3 +5,14 @@ library(knitr)
 plants <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/plants.csv')
 actions <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/actions.csv')
 threats <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/threats.csv')
+
+data("msleep")
+View(msleep)
+
+data("msleep")
+Tabla <- msleep %>% 
+  group_by(vore) %>% 
+  summarise_at("sleep_total",.funs = list(Mean = mean, SD = sd)) %>%
+  dplyr::filter(!is.na(vore)) %>% 
+  arrange(desc(Mean))
+View(Tabla)
